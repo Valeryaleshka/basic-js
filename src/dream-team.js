2522aa4612fa7cbbc7e4addfec10f2f75db0e5cc
Valeryaleshka <valeryaleshka@gmail.com>
2020-09-28 16:38:22 +0300

    fix: third task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)){
+    return false;
+  }
+
+  let dreamTeam = "";
+  members.map(name => {
+    if(typeof name == 'string'){
+      dreamTeam = dreamTeam.concat(name.trim().substr(0,1))
+    }
+  })
+
+  if(dreamTeam===""){
+    return false;
+  }
+  return dreamTeam.toUpperCase().split('').sort().join(''); 
 };
