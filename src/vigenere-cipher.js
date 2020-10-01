874081796ba1289a27c73e577a6115359bcecf36
Valeryaleshka <valeryaleshka@gmail.com>
2020-10-01 22:50:43 +0300

    feat: crypt almost done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
+
+  constructor(direction = true) {
+    (this.direction = direction) ? 'direct' : 'reverse'
+  }
+
+  encrypt(message, key) { 
+      if (message === undefined || key === undefined) {
+      throw new Error();
+    };
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (message.charAt(i).match(/^[A-Z]$/)) {
+        let letterNumber = ((message.charCodeAt(i)) + key.charCodeAt(j % key.length)-130) % 26;
+        let letter = alphabet.charAt(letterNumber);
+        result = result.concat(letter);
+        j++;
+      } else {
+        result = result.concat(message.charAt(i));
+        
+      }
+    }
   
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.direction === 'reverse'? result.split('').reverse().join(''): result;
+  }
+
+
+  decrypt(message, key) {    
+    if (message === undefined || key === undefined) {
+      throw new Error();
+    };
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (message.charAt(i).match(/^[A-Z]$/)) {
+        let letterNumber = (message.charCodeAt(i) - key.charCodeAt(j % key.length)+104) % 26;
+        let letter = alphabet.charAt(letterNumber);
+        result = result.concat(letter);
+        j++;
+      } else {
+        result = result.concat(message.charAt(i));
+        
+      }
+    }
+    return this.direction === 'reverse'? result.split('').reverse().join(''): result;
   }
 }
 
