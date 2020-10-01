7c46b4175895981ab7b935e41e7acf11501daa2e
Valeryaleshka <valeryaleshka@gmail.com>
2020-10-01 21:51:06 +0300

    feat: +3 tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,37 @@
 const CustomError = require("../extensions/custom-error");
-
+debugger;
 const chainMaker = {
+  
+  chain : [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {    
+    (value === undefined) ? this.chain.push(`'(  )'`) : this.chain.push(`( ${value} )`);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(typeof position === 'number'  &&  position > 0 && position < this.chain.length){
+    this.chain.splice((position - 1), 1);
+    return this;
+    } else {
+        this.chain = [];
+        throw new Error;
+      }    
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let arr = this.chain.join('~~');
+    this.chain = [];
+    return arr;
   }
 };
 
